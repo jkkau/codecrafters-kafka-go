044249b0242abbc9b988343075636d5a90a3810a
jkkau <jkkau10@sina.com>
2025-01-07 23:32:35 +0800

    send correlatiion id

--- app/server.go
+++ app/server.go
@@ -4,26 +4,38 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"encoding/binary"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
+func mustNot(err error, msg string) {
+	if err != nil {
+		fmt.Println(msg)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
-	if err != nil {
-		fmt.Println("Failed to bind to port 9092")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	mustNot(err, "Failed to bind to port 9092");
+
+	conn, err := l.Accept()
+	mustNot(err, "Accept error")
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	mustNot(err, "Read msg error");
+
+	responseBuf := make([]byte, 8)
+	binary.BigEndian.PutUint32(responseBuf[0:4], 0)
+	binary.BigEndian.PutUint32(responseBuf[4:8], 7)
+
+	_, err = conn.Write(responseBuf)
+	mustNot(err, "Write response error")
 }
