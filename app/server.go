a3463cec7801c316452ae902956c8734ec888436
jkkau <jkkau10@sina.com>
2025-01-11 20:12:47 +0800

    handle APIVersions requests

--- app/server.go
+++ app/server.go
@@ -23,11 +23,17 @@ const (
 	ApiVersions uint16 = 18
 )
 
+func initSupportedApiVersions() (supportedApiVersions map[uint16][]uint16) {
+	supportedApiVersions = make(map[uint16][]uint16)
+	supportedApiVersions[ApiVersions] = []uint16{4}	
+
+	return supportedApiVersions
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	supportedApiVersions := make(map[uint16][]uint16)
-	supportedApiVersions[ApiVersions] = []uint16{4}
+	supportedApiVersions := initSupportedApiVersions()
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	mustNot(err, "Failed to bind to port 9092");
@@ -45,11 +51,21 @@ func main() {
 	corelation_id := binary.BigEndian.Uint32(buf[8:])
 
 	// construct response
-	responseBuf := make([]byte, 10)
-	binary.BigEndian.PutUint32(responseBuf[0:4], 0)
+	/*
+	* Response body for apiKey 18:
+	* ApiVersions Response (Version: 0) => error_code [api_versions] 
+  	* error_code => INT16
+    * api_versions => api_key min_version max_version 
+    *   api_key => INT16
+    *   min_version => INT16
+    *   max_version => INT16
+	*
+	*/
+	responseBuf := make([]byte, 16)
+	binary.BigEndian.PutUint32(responseBuf[0:4], 16) // message size
 	binary.BigEndian.PutUint32(responseBuf[4:8], corelation_id)
 
-	error_code := uint16(35)  // not supported
+	error_code := uint16(35)  // code 35 means not supported
 	versions, ok := supportedApiVersions[request_api_key]
 	if ok {
 		for _, v := range versions {
@@ -58,7 +74,11 @@ func main() {
 			}
 		}
 	}
+
 	binary.BigEndian.PutUint16(responseBuf[8:], error_code)
+	binary.BigEndian.PutUint16(responseBuf[10:], 18)   // api_key
+	binary.BigEndian.PutUint16(responseBuf[12:], 4)    // min_version
+	binary.BigEndian.PutUint16(responseBuf[14:], 4)    // max_version
 
 	// send message
 	_, err = conn.Write(responseBuf)
