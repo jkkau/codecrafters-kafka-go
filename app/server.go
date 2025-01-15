b9840bded065284342c87c489ec48c1c4a15af55
jkkau <jkkau10@sina.com>
2025-01-15 16:55:20 +0800

    Include DescribeTopicPartitions in APIVersions

--- app/server.go
+++ app/server.go
@@ -1,15 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
-	"encoding/binary"
 )
-
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func mustNot(err error, msg string) {
 	if err != nil {
@@ -25,7 +21,7 @@ const (
 
 func initSupportedApiVersions() (supportedApiVersions map[uint16][]uint16) {
 	supportedApiVersions = make(map[uint16][]uint16)
-	supportedApiVersions[ApiVersions] = []uint16{4}	
+	supportedApiVersions[ApiVersions] = []uint16{4}
 
 	return supportedApiVersions
 }
@@ -36,14 +32,14 @@ func main() {
 	supportedApiVersions := initSupportedApiVersions()
 
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
-	mustNot(err, "Failed to bind to port 9092");
+	mustNot(err, "Failed to bind to port 9092")
 
 	conn, err := l.Accept()
 	mustNot(err, "Accept error")
 
 	buf := make([]byte, 1024)
 	_, err = conn.Read(buf)
-	mustNot(err, "Read msg error");
+	mustNot(err, "Read msg error")
 
 	// parse received data
 	request_api_key := binary.BigEndian.Uint16(buf[4:])
@@ -53,21 +49,11 @@ func main() {
 	// construct response
 	// https://kafka.apache.org/protocol.html#The_Messages_ApiVersions
 	// https://forum.codecrafters.io/t/question-about-handle-apiversions-requests-stage/1743/5
-	/*
-	* Response body for apiKey 18:
-	* ApiVersions Response (Version: 0) => error_code [api_versions] 
-  	* error_code => INT16
-    * api_versions => api_key min_version max_version 
-    *   api_key => INT16
-    *   min_version => INT16
-    *   max_version => INT16
-	*
-	*/
-	responseBuf := make([]byte, 23)
-	binary.BigEndian.PutUint32(responseBuf[0:4], 23-4) // message size = sizeof(Header)+sizeof(Body)
+	responseBuf := make([]byte, 27)
+	binary.BigEndian.PutUint32(responseBuf[0:4], 27-4) // message size = sizeof(Header)+sizeof(Body)
 	binary.BigEndian.PutUint32(responseBuf[4:8], corelation_id)
 
-	error_code := uint16(35)  // code 35 means not supported
+	error_code := uint16(35) // code 35 means not supported
 	versions, ok := supportedApiVersions[request_api_key]
 	if ok {
 		for _, v := range versions {
@@ -78,13 +64,16 @@ func main() {
 	}
 
 	binary.BigEndian.PutUint16(responseBuf[8:], error_code)
-	responseBuf[10] = 2  // (number+1) of api_keys
-	binary.BigEndian.PutUint16(responseBuf[11:], 18)   // api_key
-	binary.BigEndian.PutUint16(responseBuf[13:], 4)    // min_version
-	binary.BigEndian.PutUint16(responseBuf[15:], 4)    // max_version
-	responseBuf[17] = 0  // _tagged_fields
-	binary.BigEndian.PutUint16(responseBuf[18:], 0)    // throttle_time_ms
-	responseBuf[22] = 0  // _tagged_fields
+	responseBuf[10] = 3                              // (number+1) of api_keys
+	binary.BigEndian.PutUint16(responseBuf[11:], 18) // api_key APIVersions
+	binary.BigEndian.PutUint16(responseBuf[13:], 0)  // min_version
+	binary.BigEndian.PutUint16(responseBuf[15:], 4)  // max_version
+	binary.BigEndian.PutUint16(responseBuf[17:], 75) // api_key DescribeTopicPartitions
+	binary.BigEndian.PutUint16(responseBuf[19:], 0)  // min_version
+	binary.BigEndian.PutUint16(responseBuf[21:], 0)  // max_version
+	responseBuf[23] = 0                              // _tagged_fields
+	binary.BigEndian.PutUint16(responseBuf[14:], 0)  // throttle_time_ms
+	responseBuf[26] = 0                              // _tagged_fields
 
 	// send message
 	_, err = conn.Write(responseBuf)
